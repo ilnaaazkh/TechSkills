e99c6bc5b19162694b0ea870b860657f625cd301
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2024-03-16 19:31:48 +0300

    Make a scaffold for clean architecture

---
+++ TechSkills.Backend/TechSkills.Domain/Course.cs
@@ -0,0 +1,39 @@
+﻿namespace TechSkills.Domain
+{
+    public class Course
+    {
+        Guid Guid { get; }
+        public string Title { get; }
+
+        public string Description { get; }
+
+        public const int MAX_TITLE_LENGTH = 50;
+        public const int MAX_DESCRIPTION_LENGTH = 300;
+
+        private Course(Guid guid, string title, string description)
+        {
+            Guid = guid;
+            Title = title;
+            Description = description;
+        }
+
+        public static (Course course, string Error) Create(Guid guid, string title, string description)
+        {
+            var error = string.Empty;
+
+            if(string.IsNullOrEmpty(title) || title.Length > MAX_TITLE_LENGTH)
+            {
+                error += "Title can not be empty or longer then 50 symbols. ";
+            }
+
+            if (string.IsNullOrEmpty(description) || description.Length > MAX_DESCRIPTION_LENGTH)
+            {
+                error += "Title can not be empty or longer then 300 symbols. ";
+            }
+
+            var course = new Course(guid, title, description);
+
+            return (course, error);
+        }
+    }
+}
