22edab88728b332ca258d1d8fe513c07fa602974
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2024-03-30 23:16:06 +0300

    Add module and lesson in Domain

--- TechSkills.Backend/TechSkills.Domain/Course.cs
+++ TechSkills.Backend/TechSkills.Domain/Course.cs
@@ -1,15 +1,25 @@
-﻿namespace TechSkills.Domain
+﻿using CSharpFunctionalExtensions;
+
+namespace TechSkills.Domain
 {
     public class Course
     {
-        public Guid Id { get; }
-
-        public string Title { get; }
-
-        public string Description { get; }
-
         public const int MAX_TITLE_LENGTH = 50;
         public const int MAX_DESCRIPTION_LENGTH = 300;
+
+
+        private readonly List<Module> modules = new List<Module>();
+        /*private readonly List<User> students = new List<User>();
+        private readonly List<User> authors = new List<User>();*/
+
+
+        public Guid Id { get; }
+        public string Title { get; }
+        public string Description { get; }
+        public IReadOnlyCollection<Module> Modules => modules;
+        /*public IReadOnlyCollection<User> Students => students;
+        public IReadOnlyCollection<User> Authors => authors;*/
+
 
         private Course(Guid guid, string title, string description)
         {
@@ -17,24 +27,47 @@
             Title = title;
             Description = description;
         }
+        public void AddModules(Module module)
+        {
+            modules.Add(module);
+        }
+        /*public void AddAuthors(IEnumerable<User> newAuthors)
+        {
+            foreach (var author in newAuthors)
+            {
+                authors.Add(author);
+            }
+        }*/
+        /*public void AddStudents(IEnumerable<User> newStudents)
+        {
+            foreach (var student in newStudents)
+            {
+                students.Add(student);
+            }
+        }*/
 
-        public static (Course Course, string Error) Create(Guid guid, string title, string description)
+        public static Result<Course> Create(Guid guid, string title, string description)
         {
             var error = string.Empty;
 
-            if(string.IsNullOrEmpty(title) || title.Length > MAX_TITLE_LENGTH)
+            if (string.IsNullOrEmpty(title) || title.Length > MAX_TITLE_LENGTH)
             {
-                error += "Title can not be empty or longer then 50 symbols. ";
+                error += $"Title can not be empty or longer than {MAX_TITLE_LENGTH} symbols. ";
             }
 
             if (string.IsNullOrEmpty(description) || description.Length > MAX_DESCRIPTION_LENGTH)
             {
-                error += "Title can not be empty or longer then 300 symbols. ";
+                error += $"Title can not be empty or longer than {MAX_DESCRIPTION_LENGTH} symbols. ";
+            }
+
+            if(error != string.Empty)
+            {
+                return Result.Failure<Course>(error);
             }
 
             var course = new Course(guid, title, description);
 
-            return (course, error);
+            return Result.Success<Course>(course);
         }
     }
 }
