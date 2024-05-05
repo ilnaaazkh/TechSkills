7a4e83d7b953b8fe333aa082892b2d34a6eb738a
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2024-05-05 14:31:00 +0300

    Исправить конфигурацию моделей

---
+++ TechSkills.Backend/TechSkills.DataAccess/Configuration/ModuleConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Xml.Linq;
+using TechSkills.DataAccess.Entities;
+using TechSkills.Domain;
+
+namespace TechSkills.DataAccess.Configuration
+{
+    public class ModuleConfiguration : IEntityTypeConfiguration<ModuleEntity>
+    {
+        public void Configure(EntityTypeBuilder<ModuleEntity> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Title)
+                .IsRequired()
+                .HasMaxLength(Module.MODULE_TITLE_MAX_LENGTH);
+
+            builder
+                .HasOne(x => x.Course); //TODO: Configure this 
+        }
+    }
+}
