a353ef53ceffd109b2901d2325fa4f1235cbc66a
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2024-05-05 17:29:19 +0300

    Добавить порядковый номер в сущности Урок и Модуль в слое Domain

--- TechSkills.Backend/TechSkills.Domain/Lesson.cs
+++ TechSkills.Backend/TechSkills.Domain/Lesson.cs
@@ -8,15 +8,17 @@ namespace TechSkills.Domain
         public Guid Id { get; }
         public string Title { get; }
         public string Content { get; }
+        public int OrderNumber { get; set; }
 
-        private Lesson(Guid Id, string Title, string Content)
+        private Lesson(Guid Id, string Title, string Content, int OrderNumber)
         {
             this.Id = Id;
             this.Title = Title; 
             this.Content = Content;
+            this.OrderNumber = OrderNumber;
         }
 
-        public static Result<Lesson> Create(Guid Id, string Title, string Content)
+        public static Result<Lesson> Create(Guid Id, string Title, string Content, int OrderNumber)
         {
             var error = string.Empty;
 
@@ -25,12 +27,17 @@ namespace TechSkills.Domain
                 error += $"Lesson title can't be empty or longer than {LESSON_TITLE_MAX_LENGTH} ";
             }
 
-            if (string.IsNullOrEmpty(error))
+            if( OrderNumber < 0)
+            {
+                error += $"Lesson order number must be non-negative integer number";
+            }
+
+            if (!string.IsNullOrEmpty(error))
             {
                 return Result.Failure<Lesson>(error);
             }
-            var lesson = new Lesson(Id, Title, Content);
 
+            var lesson = new Lesson(Id, Title, Content, OrderNumber);
             return Result.Success<Lesson>(lesson);
         }
     }
--- TechSkills.Backend/TechSkills.Domain/Module.cs
+++ TechSkills.Backend/TechSkills.Domain/Module.cs
@@ -10,14 +10,16 @@ namespace TechSkills.Domain
         public Guid Id { get; }
         public string Title { get; }
         public IReadOnlyCollection<Lesson> Lessons => lessons;
+        public int OrderNumber { get; }
 
-        private Module(Guid Id, string title)
+        private Module(Guid Id, string title, int OrderNumber)
         {
             this.Id = Id;
             Title = title;
+            this.OrderNumber = OrderNumber;
         }
 
-        public static Result<Module> Create(Guid Guid, String Title)
+        public static Result<Module> Create(Guid Guid, String Title, int OrderNumber)
         {
             var error = string.Empty;
 
@@ -26,12 +28,17 @@ namespace TechSkills.Domain
                 error += $"Module title can't be empty or longer than {MODULE_TITLE_MAX_LENGTH}";
             }
 
-            if (error != string.Empty)
+			if (OrderNumber < 0)
+			{
+				error += $"Module order number must be non-negative integer number";
+			}
+
+			if (!string.IsNullOrEmpty(error))
             {
                 return Result.Failure<Module>(error);
             }
 
-            var module = new Module(Guid, Title);
+            var module = new Module(Guid, Title, OrderNumber);
 
             return Result.Success<Module>(module);
         }
