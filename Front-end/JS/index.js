29fa4d6dda85eaab804ae6a306ff4e1ac1392eeb
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2024-08-18 17:28:45 +0300

    delete frontend dir

--- Front-end/JS/index.js
+++
@@ -1,8 +0,0 @@
-import { loadHeader, loadMain } from "./modules/utils.js";
-import { getAllCourses } from "./modules/courses.js";
-
-document.addEventListener("DOMContentLoaded", async () => {
-  await loadHeader();
-  await loadMain();
-  getAllCourses();
-});
